d39d52ae4dd3f7ac5193ba38db0a1e73e35ab64b
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-02-18 01:17:24 +0800

    Add convnext perceptual loss
    https://github.com/sypsyp97/convnext_perceptual_loss

--- src/hakulatent/losses/perceptual.py
+++ src/hakulatent/losses/perceptual.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import lpips
 from torchvision import models
+from convnext_perceptual_loss import ConvNextPerceptualLoss, ConvNextType
 
 
 class VGGFeatureExtractor(nn.Module):
@@ -44,3 +45,28 @@ class LPIPSLoss(nn.Module):
     def forward(self, x_real, x_recon):
         loss = self.lpips_model(x_real, x_recon)
         return loss.mean()
+
+
+class ConvNeXtPerceptualLoss(nn.Module):
+    def __init__(
+        self,
+        model_type=ConvNextType.BASE,
+        feature_layers=[0, 2, 4, 6, 8, 10, 12, 14],
+        feature_weights=None,
+        use_gram=False,
+        input_range=(0, 1),
+        layer_weight_decay=0.99,
+    ):
+        super(ConvNeXtPerceptualLoss, self).__init__()
+        self.model = ConvNextPerceptualLoss(
+            model_type=model_type,
+            feature_layers=feature_layers,
+            feature_weights=feature_weights,
+            use_gram=use_gram,
+            input_range=input_range,
+            layer_weight_decay=layer_weight_decay,
+        )
+
+    def forward(self, x_real, x_recon):
+        loss = self.model(x_real, x_recon)
+        return loss
