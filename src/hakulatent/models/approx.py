7bbe791d39254f605e732b320dde79e1b430aebe
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-02-20 18:08:07 +0800

    Update approx.py

--- src/hakulatent/models/approx.py
+++ src/hakulatent/models/approx.py
@@ -6,9 +6,13 @@ import torch.nn.functional as F
 class LatentApproxDecoder(nn.Module):
     def __init__(self, latent_dim=4, out_channels=3, shuffle=2):
         super().__init__()
-        self.conv_in = nn.Conv2d(latent_dim, out_channels * shuffle**2, 5, stride=1, padding=2)
+        self.conv_in = nn.Conv2d(
+            latent_dim, out_channels * shuffle**2, 5, stride=1, padding=2
+        )
         self.conv_out = nn.Conv2d(out_channels, out_channels, 5, stride=1, padding=2)
         self.shuffle = shuffle
+        nn.init.zeros_(self.conv_out.bias)
+        nn.init.zeros_(self.conv_out.weight)
 
     def forward(self, x):
         x = self.conv_in(x)
