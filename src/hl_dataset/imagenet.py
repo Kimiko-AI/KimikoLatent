1b6cecebb885ea271d377cb1d7810b520fb18708
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-02-26 17:43:59 +0800

    Update imagenet.py

--- src/hl_dataset/imagenet.py
+++ src/hl_dataset/imagenet.py
@@ -3,10 +3,11 @@ from datasets import load_dataset
 
 
 class ImageNetDataset(data.Dataset):
-    def __init__(self, split, transform=None):
+    def __init__(self, split, transform=None, max_len=None):
         self.split = split
         self.transform = transform
         self.dataset = load_dataset("evanarlian/imagenet_1k_resized_256", split=split)
+        self.max_len = max_len
 
     def __getitem__(self, index):
         entry = self.dataset[index]
@@ -19,4 +20,4 @@ class ImageNetDataset(data.Dataset):
         return img, target
 
     def __len__(self):
-        return len(self.dataset)
+        return self.max_len or len(self.dataset)
